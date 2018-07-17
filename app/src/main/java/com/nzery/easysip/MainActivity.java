8a191c86467e456353eda0f7f6bdd26ae33b1764
dengyubing <dengyubing@star-net.cn>
2018-07-17 13:55:23 +0800

    移除camera调试代码

--- app/src/main/java/com/nzery/easysip/MainActivity.java
+++ app/src/main/java/com/nzery/easysip/MainActivity.java
@@ -1,113 +1,22 @@
 package com.nzery.easysip;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.graphics.SurfaceTexture;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CaptureRequest;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Surface;
-import android.view.TextureView;
 
-import java.util.Collections;
 
-public class MainActivity extends Activity implements TextureView.SurfaceTextureListener {
+public class MainActivity extends Activity{
 
-    private TextureView mTextureView;
     private static final String TAG = MainActivity.class.getSimpleName();
-    private CameraDevice mCameraDevice;
-    private CameraCaptureSession mPreviewSession;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextureView = findViewById(R.id.myTextureView);
-        mTextureView.setSurfaceTextureListener(this);
 
-    }
-
-    @Override
-    public void onSurfaceTextureAvailable(final SurfaceTexture surfaceTexture, int i, int i1) {
-        surfaceTexture.setDefaultBufferSize(1920,1080);
-        CameraManager manager = (CameraManager) this.getSystemService(Context.CAMERA_SERVICE);
-        try {
-            String cameraId = manager.getCameraIdList()[0];
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-            manager.openCamera(cameraId, new CameraDevice.StateCallback() {
-                @Override
-                public void onOpened(@NonNull CameraDevice camera) {
-                    try {
-                        mCameraDevice = camera;
-                        final CaptureRequest.Builder previewBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-                        Surface previewSurface = new Surface(surfaceTexture);
-                        previewBuilder.addTarget(previewSurface);
-                        mCameraDevice.createCaptureSession(Collections.singletonList(previewSurface), new CameraCaptureSession.StateCallback() {
-                            @Override
-                            public void onConfigured(@NonNull CameraCaptureSession session) {
-                                try {
-                                    mPreviewSession = session;
-                                    mPreviewSession.setRepeatingRequest(previewBuilder.build(), null, null);
-                                } catch (CameraAccessException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-
-                            @Override
-                            public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-
-                            }
-                        }, null);
-                    } catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void onDisconnected(@NonNull CameraDevice camera) {
-
-                }
-
-                @Override
-                public void onError(@NonNull CameraDevice camera, int error) {
-
-                }
-            }, null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
-
-    }
-
-    @Override
-    public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
-        if (mPreviewSession != null) {
-            mPreviewSession.close();
-            mPreviewSession = null;
-        }
-        if (mCameraDevice != null) {
-            mCameraDevice.close();
-            mCameraDevice = null;
-        }
-        return true;
-    }
-
-    @Override
-    public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
-        Log.d(TAG,"onSurfaceTextureUpdated");
     }
 }
